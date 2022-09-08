4fd1113e5dcc764af16e4c1a2326f65a7c236d8e
Stan <stanlis@mail.ru>
2022-09-08 22:22:01 +0300

    Add 19,21,23

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,17 +2,18 @@
 // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 Console.Clear();
-Console.Write("Введите число: ");
+Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int i = 2;
-while (i <= n )
+
+int n0 = 1;
+int n1 = 2;
+while ( n0 <= n);
 {
-    Console.WriteLine($"{i} ");
-    i+=2;
+    Console.WriteLine(n0);
+    n0= n0+2;
+
 }
 
 
 
-
-
-
+    // n%2==0, n =n -2;
---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти (1-4)");
+string quarter = Console.ReadLine();
+
+
+string Quarter(string quart1)
+{
+    if (quart1 == "1") return "Первая четверть";
+    if (quart1 == "2") return "Вторая четверть";
+    if (quart1 == "3") return "Третья четверть";
+    if (quart1 == "4") return "Четвертая четверть";
+    return "Введен некорретный номер четверти";
+
+}
+string result = Quarter (quarter);
+Console.WriteLine(result);
